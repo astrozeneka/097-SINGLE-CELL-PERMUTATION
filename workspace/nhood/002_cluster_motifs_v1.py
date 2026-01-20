e2704b96ad13fb72f2ae8581030b5a1af92e0ac9
Ryan Rasoarahona <ryanrasoarahona@gmail.com>
2026-01-20 10:24:06 +0700

    The script 002 can now correctly exclude columns

--- workspace/nhood/002_cluster_motifs_v1.py
+++ workspace/nhood/002_cluster_motifs_v1.py
@@ -9,11 +9,25 @@ parser.add_argument('--input', type=str, default="nhood_results/all_k12.csv", he
 parser.add_argument('--n-motifs', type=int, default=24, help="Number of motifs to cluster")
 parser.add_argument('--knn-seed', type=int, default=42, help="Random seed for KMeans")
 parser.add_argument("--output", type=str, required=True, help="Output file name")
+
+# Column selectors
+parser.add_argument('--phenotype-column', type=str, default='Phenotype', help="Column name for Phenotype")
+parser.add_argument('--imageid-column', type=str, default='imageid', help="Column name for Image ID")
+parser.add_argument('--x-coord-column', type=str, default='Centroid X', help="Column name for X coordinate")
+parser.add_argument('--y-coord-column', type=str, default='Centroid Y', help="Column name for Y coordinate")
+parser.add_argument('--region-column', type=str, default='Parent Region', help="Column name for Region")
+
 args = parser.parse_args()
 
 if __name__ == '__main__':
     nhood_res = pd.read_csv(args.input, index_col=0)
-    nhood_vec = nhood_res.drop(columns=['Phenotype', 'imageid'])
+
+    # Columns to exclude from clustering (informative/metadata columns)
+    exclude_cols = [args.phenotype_column, args.imageid_column,
+                    args.x_coord_column, args.y_coord_column,
+                    args.region_column]
+
+    nhood_vec = nhood_res.drop(columns=exclude_cols, errors='ignore')
     kmeans = KMeans(n_clusters=args.n_motifs, random_state=args.knn_seed)
     motifs = kmeans.fit_predict(nhood_vec)
     nhood_res['motif'] = motifs
