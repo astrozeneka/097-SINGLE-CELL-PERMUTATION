5f6fb417c5373a0140f6ebf66fa290dea164878c
Ryan Rasoarahona <ryanrasoarahona@gmail.com>
2026-03-31 15:31:08 +0700

    WIP: add starter-code for brush-2d-v2-2

--- src/app/cell-annotator/brush-2d-v2-2/page.tsx
+++ src/app/cell-annotator/brush-2d-v2-2/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 
+import { ScatterCanvas } from "../brush-2d-v2/scatter-canvas";
 import HorizontalSplit from "../horizontal-split";
 
 export default function Page() {
     return (
         <div style={{ width: "100vw", height: "100vh", position: "absolute", left: 0, top: 0 }}>
             <HorizontalSplit>
-                <div style={{ background: "lightblue" }}>Hello world left</div>
+                <div style={{ background: "lightblue" }}>
+                    <ScatterCanvas
+                        key={lcSubset}
+                        data={pointsDataSubset}
+                        xAccessor={}
+                        yAccessor={}
+                        colorEncoder={byClusterSelectionEncoderV2}
+                        size={lcSize}
+                        transform={ lcTransform }
+                        setTransform={setLcTransform}
+                        onReady={() => {}}
+                        polygonMask={polygonMask}
+                    ></ScatterCanvas>
+                </div>
                 <div style={{ background: "lightcoral" }}>Hello world right</div>
             </HorizontalSplit>
         </div>
